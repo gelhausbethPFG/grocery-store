2957c1e78f27535b7fcc821e6ce844746e67e25f
Scott Schoenleber <schoenleber.scott@principal.com>
2019-11-01 10:51:01 -0500

    fixing variable and method names

--- src/main/java/com/principal/sbd/code/interview/code/kata/Checkout.java
+++ src/main/java/com/principal/sbd/code/interview/code/kata/Checkout.java
@@ -2,7 +2,7 @@ package com.principal.sbd.code.interview.code.kata;
 
 public class Checkout {
 
-    public static int add(String numbers) {
+    public static int checkout(String products) {
         return -1;
     }
 }
